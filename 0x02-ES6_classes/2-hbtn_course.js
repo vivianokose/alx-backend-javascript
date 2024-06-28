bd768563a04047e911ac79ae32ed03fe168911f4
vivianokose <vivianokose@gmail.com>
2024-06-28 00:03:34 -0700

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,12 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._checkType(name, 'string', 'Name');
-    this._checkType(length, 'number', 'Length');
-    this._checkType(students, 'array', 'Students');
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+constructor(name, length, students) {
+  this._checkType(name, 'string', 'Name');
+  this._checkType(length, 'number', 'Length');
+  this._checkType(students, 'array', 'Students');
+  this._name = name;
+  this._length = length;
+  this._students = students;
+}
 
-  get name() {
-    return this._name;
-  }
+get name() {
+  return this._name;
+}
